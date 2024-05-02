51251f1c4bed232989bef6d1d0320cd83b118789
Jason <JPriskomatis@gmail.com>
2024-05-02 15:27:52 +0300

    added knocking on bathroom door

--- Assets/TriggerFrontDoor.cs
+++ Assets/TriggerFrontDoor.cs
@@ -5,16 +5,24 @@ using UnityEngine;
 public class TriggerFrontDoor : MonoBehaviour
 {
     [SerializeField] Door frontDoor;
+    [SerializeField] DoorKnocking knocking;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             frontDoor.CloseDoor();
+            
+            StartCoroutine(StartKnocking());
             StartCoroutine(DestroyScript());
         }
     }
 
+    private IEnumerator StartKnocking()
+    {
+        yield return new WaitForSeconds(1f);
+        knocking.StartKnocking();
+    }
     private IEnumerator DestroyScript()
     {
         yield return new WaitForSeconds(1.5f);
