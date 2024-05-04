fa98ef5ae01fad49dc349cac9bb957a54001e867
Jason <JPriskomatis@gmail.com>
2024-05-04 23:53:46 +0300

    audio for when opening toilet

--- Assets/Scripts/Objects/Toilet/ToiletScript.cs
+++ Assets/Scripts/Objects/Toilet/ToiletScript.cs
@@ -8,25 +8,37 @@ public class ToiletScript : MonoBehaviour, IInteractable
     [SerializeField] private Animator anim;
     TextAppear textAppear;
     private bool isOpen;
+    [SerializeField] private AudioSource headAudio;
+
+
     private void Start()
     {
         textAppear = GetComponent<TextAppear>();
     }
+
     public void Interact()
     {
         if (Input.GetKeyDown(KeyCode.E) && !isOpen)
         {
             anim.SetBool("Open", true);
             isOpen = true;
+            // Use FadeInAndPlay method to fade in audio and play it
+            headAudio.Play();
+        }
+        else if (Input.GetKeyDown(KeyCode.E) && isOpen)
+        {
+            anim.SetBool("Open", false);
+            isOpen = false;
+            // Use FadeOut method to fade out the audio
+            headAudio.Play();
         }
     }
 
     public void OnInteractEnter()
     {
-        if (isOpen)
+        if (!isOpen)
         {
             textAppear.SetText("Open");
-
         }
     }
 
@@ -34,6 +46,4 @@ public class ToiletScript : MonoBehaviour, IInteractable
     {
         textAppear.RemoveText();
     }
-
-    
 }
