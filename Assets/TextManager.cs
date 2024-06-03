b08af6a351a92223d175423a6d41a887c13cee12
Jason <JPriskomatis@gmail.com>
2024-06-03 23:06:35 +0300

    Intro Text

---
+++ Assets/TextManager.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class TextManager : MonoBehaviour
+{
+    [SerializeField] FadeText fadeText;
+    [SerializeField] TextMeshProUGUI textToDisplay;
+
+    private void Start()
+    {
+        StartCoroutine(FirstText());
+    }
+
+    IEnumerator FirstText()
+    {
+        yield return new WaitForSeconds(3f);
+        textToDisplay.text = "This game contains scenes of explicit violence and gore.";
+        fadeText.FadeInText();
+        yield return new WaitForSeconds(fadeText.fadeDuration);
+        StartCoroutine(FirstTextDisappear());
+    }
+
+    IEnumerator FirstTextDisappear()
+    {
+        yield return new WaitForSeconds(4f);
+        fadeText.FadeOutText();
+        
+        StartCoroutine(SecondarySpriteTexture());
+    }
+
+    IEnumerator SecondarySpriteTexture()
+    {
+        yield return new WaitForSeconds(fadeText.fadeDuration);
+        textToDisplay.text = " If you have a history of heart conditions, epilepsy, or are prone to panic attacks, we strongly advise against playing this game.";
+        fadeText.FadeInText();
+        yield return new WaitForSeconds(fadeText.fadeDuration);
+        StartCoroutine(SecondTextDisappear());
+    }
+
+    IEnumerator SecondTextDisappear()
+    {
+        yield return new WaitForSeconds(4f);
+        fadeText.FadeOutText();
+        StartCoroutine(ThirdTextDisappear());
+
+
+    }
+
+    IEnumerator ThirdTextDisappear()
+    {
+        yield return new WaitForSeconds(fadeText.fadeDuration);
+        textToDisplay.text = "For the best experience, we recommend playing with headphones.";
+        fadeText.FadeInText();
+        yield return new WaitForSeconds(fadeText.fadeDuration);
+        fadeText.FadeOutText();
+    }
+}
