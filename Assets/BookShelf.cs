f6e323ef75c73f41f6e04b11ce4fe86e9bbc1b62
Jason <JPriskomatis@gmail.com>
2024-07-06 22:35:36 +0300

    Add: Secret room
    Added bookshelf and switch button functionality to reveal the secret room

---
+++ Assets/BookShelf.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BookShelf : MonoBehaviour, IInteractable
+{
+    bool moveBooks = false;
+    [SerializeField] Animator anim;
+
+    [SerializeField] SwitchButton btn;
+    bool firstInteract = true;
+
+    [SerializeField] AudioSource audioSource;
+
+    public void Interact()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+            {
+            if (!moveBooks)
+            {
+                TextAppear.RemoveText();
+
+
+                TextAppear.SetText("There's a switch there");
+                StartCoroutine(TextForSwitch());
+                
+            }
+            else
+            {
+                //MOVE Books;
+                anim.SetTrigger("move");
+                audioSource.Play();
+
+                btn.GetComponent<SwitchButton>().enabled = true;
+
+                Component scriptToDestroy = this.GetComponent<BookShelf>();
+                Destroy(scriptToDestroy);
+            }
+        }
+        
+        
+    }
+
+    public void OnInteractEnter()
+    {
+        if (firstInteract)
+        {
+            TextAppear.SetText("Inspect");
+            firstInteract = false;
+
+        } else
+            TextAppear.SetText("Move books");
+
+    }
+
+    public void OnInteractExit()
+    {
+        TextAppear.RemoveText();
+    }
+
+    IEnumerator TextForSwitch()
+    {
+        yield return new WaitForSeconds(1.5f);
+        TextAppear.SetText("Move books");
+        moveBooks = true;
+    }
+}
